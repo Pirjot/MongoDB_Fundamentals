719ea5982c4e530445812e4449029c4bef4f3484
Pirjot <apirjot@gmail.com>
2021-11-01 00:23:53 -0700

    Final MongoDB version finished

--- app.js
+++ app.js
@@ -9,7 +9,6 @@ const routes = require('./routes.js');
 
 app.use(express.json());
 app.use(express.static('public'));
-app.use(mongo.connectClient);
 
 //Perform a push to the server based on user provided data.
 app.post('/database', (req, res) => routes.database(req, res));
@@ -18,6 +17,51 @@ app.post('/database', (req, res) => routes.database(req, res));
 app.get('/get_data', (req, res) => routes.get_data(req, res));
 
 let server = app.listen(3000, () => console.log("Listening..."));
+
+/**
+ * A test function to ensure that MongoDB is running correctly
+ * and that all functionality is working.
+ */
+async function testMongo() {
+    let response = null;
+
+    console.log("Running Test on Mongo Capabilities...");
+
+    console.log("Attempting connect, the following value should be true:");
+    response = await mongo.connectClient();
+    console.log(response);
+
+    console.log("Attempting second connect, the following value should be false:");
+    response = await mongo.connectClient(); //If connection is attempted twice, this returns false
+    console.log(response);
+
+    console.log("Attempting add value...");
+    response = await mongo.add_data({"value":"VALUE", "data":15.31}, "test", "test"); //Attempt adding a doc
+    console.log(response);    
+
+    console.log("Attempting second add value (this time with id 12345...)");
+    response = await mongo.add_data({_id: "12345", "value":"VALUE"}, "test", "test"); //Attempt adding a doc with an id
+    console.log(response);
+
+    console.log("Attempting to retrieve all docs in the test/test collection...");
+    response = await mongo.get_data({}, "test", "test");
+    console.log(response);
+
+    console.log("Deleting the object with the unique _id 12345...");
+    response = await mongo.delete_doc_id("12345", "test", "test");
+    console.log(response);
+    console.log(await mongo.get_data({}, "test", "test"));
+
+    console.log("Updating all docs that have 'VALUE' as their value to increment their data by 5");
+    response = await mongo.update_docs({"value": "VALUE"}, {$inc: {"data":5}}, "test", "test");
+    console.log(response);
+    console.log(await mongo.get_data({}, "test", "test"));
+
+    console.log("Clearing the test/test collection...");
+    response = await mongo.delete_docs_q({}, "test", "test");
+    console.log(await mongo.get_data({}, "test", "test"));
+}
+testMongo();
 
 //On server/process closing, ensure that the MongoDB connection was terminated
 process.on('SIGINT', () => {
--- mongodb-pirjot.js
+++ mongodb-pirjot.js
@@ -1,24 +1,47 @@
+/**
+ * A collection of useful MongoDB functions that can be used as a template for
+ * any NodeJS server (intended initially for use by Ohlone's ./MakeMyFuture team).
+ * 
+ * You need to know these two pages are available as resources:
+ * How to build a Query: https://docs.mongodb.com/drivers/node/current/fundamentals/crud/query-document/
+ * How to build a Filter (for update functions): https://docs.mongodb.com/drivers/node/current/fundamentals/crud/write-operations/change-a-document/
+ * 
+ * @author Pirjot Atwal
+ * @file mongodb-pirjot.js
+ */
+
 // Import mongodb (make sure you do npm install mongodb first)
 const { MongoClient } = require('mongodb');
 // Build a uri, user and password settings are configured on the website
 const uri = "mongodb+srv://" + process.env.USERNAME_PASSWORD + "@mongodbtest.v5yrf.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
 // Configure client, pass in uri and options
-let client = undefined;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 
 /**
- * Begin connection to Mongo Database. Make sure to call closeClient at some.
+ * Begin connection to Mongo Database. Make sure to call closeClient at some point.
+ * You can await this function so as to guarantee the client is connected before performing
+ * any operations. Written as a middleware function but does not necessarily need to
+ * be used as such.
+ * 
+ * @return {MongoClient} The connected client.
  */
-async function connectClient(req, res, next) {
-    if (client == undefined) {
-        console.log("Starting connection with database.")
-        client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true })
-        await client.connect();
+async function connectClient(req, res, next=()=>{}) {
+    if (!module.exports.isConnected) {
+        console.log("Connecting to database...");
+        module.exports.isConnected = true;
+        await module.exports.client.connect();
+    } else {
+        console.log("Already Connected, ignoring request. (Please comment this line in official versions.)");
+        next();
+        return false;
     }
     next();
+    return true;
 }
 
 /**
- * Terminate client connection, client object is unusable now
+ * Terminate client connection, client object is unusable now.
+ * Make sure this function is called at some point near the end of the program.
  */
 async function closeClient() {
     console.log("\nTerminating connection with database...")
@@ -26,9 +49,9 @@ async function closeClient() {
 }
 
 /**
- * Push a value into the database.
+ * Push a doc into the database.
  * 
- * @param {JSON} value The value you want to insert
+ * @param {JSON} value The value/document/JSON object you want to insert
  * @param {String} database
  * @param {String} collection 
  */
@@ -38,18 +61,63 @@ async function add_data(value, database = "default", collection = "default") {
 }
 
 /**
- * Get all collections from the database
+ * Get all collections that match a query from the database collection.
+ * Note: provide an empty JSON Object if you would like to get all data.
  * 
+ * @param {JSON} query Use an empty doc to select everything
  * @param {String} database
  * @param {String} collection
+ * @return {Array} An array of all documents.
  */
-async function get_data(database = "default", collection = "default") {
-    let response = await client.db(database).collection(collection).find({}); //Empty Query select everything
-    return await response.toArray();
+async function get_data(query = {}, database = "default", collection = "default") {
+    let response = client.db(database).collection(collection).find(query);
+    return response.toArray();
 }
 
+/**
+ * Delete the doc in the given collection with the matching unique _id.
+ * 
+ * @param {String} _id
+ * @param {String} database
+ * @param {String} collection
+ * @returns {DeleteResult}
+ */
+async function delete_doc_id(_id, database = "default", collection = "default") {
+    let response = await client.db(database).collection(collection).deleteOne({"_id": _id});
+    return response;
+}
 
+/**
+ * Delete all docs that match a given query. Look at the query rules to make a needed query.
+ * NOTE: An empty query will delete all docs.
+ * 
+ * @param {JSON} query 
+ * @param {String} database 
+ * @param {String} collection 
+ * @returns {DeleteResult}
+ */
+async function delete_docs_q(query, database = "default", collection = "default") {
+    let response = await client.db(database).collection(collection).deleteMany(query);
+    return response;
+}
+
+/**
+ * One of the most advanced of MongoDB operations. 
+ * Takes a filter and an update object to update all docs that satisfy
+ * the filter with the update.
+ * 
+ * @param {JSON} filter For example {name: "Bob Smith"}
+ * @param {JSON} update For example {$inc: num} (Increments num field in the doc)
+ * @param {String} database 
+ * @param {String} collection 
+ * @returns {DeleteResult}
+ */
+ async function update_docs(filter, update, database = "default", collection = "default") {
+    let response = await client.db(database).collection(collection).updateMany(filter, update);
+    return response;
+}
 
 module.exports = {
-    client, connectClient, closeClient, add_data, get_data
+    client, isConnected:false, connectClient, closeClient, add_data, get_data, delete_doc_id,
+    delete_docs_q, update_docs
 };
